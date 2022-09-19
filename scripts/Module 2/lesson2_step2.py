7cddb015d664c3859b5e7a8957b0e778c5c610ae
AshA <b.akhmadjonov@student.inha.uz>
2022-09-19 18:38:26 +0500

    Dropdown list practice scripts. Using W3School.

--- scripts/Module 2/lesson2_step2.py
+++ scripts/Module 2/lesson2_step2.py
@@ -1,15 +1,22 @@
+from tkinter.tix import Select
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import math
 
-link = ""
+# link = "http://suninjuly.github.io/selects1.html"
+# link = "http://suninjuly.github.io/selects2.html"
+link = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select"
+
+
 
 try:
     browser = webdriver.Chrome()
     browser.get(link)
+    select = Select(browser.find_element(By.ID, "cars"))
+    select.select_by_value("2") # searching element 1
 
-
+    press_button = browser.find_element(By.CSS_SELECTOR, "[type = "submit"]")
 
 finally:
     time.sleep(5)
