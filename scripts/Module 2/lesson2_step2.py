6b837fb80cca575f9a5740441b03bb9f75cebbc9
AshA <b.akhmadjonov@student.inha.uz>
2022-09-19 20:49:05 +0500

    Working with dropdown list.

--- scripts/Module 2/lesson2_step2.py
+++ scripts/Module 2/lesson2_step2.py
@@ -1,22 +1,20 @@
-from tkinter.tix import Select
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import math
+from tkinter import *
+import tkinter
 
 # link = "http://suninjuly.github.io/selects1.html"
 # link = "http://suninjuly.github.io/selects2.html"
 link = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select"
 
-
-
 try:
     browser = webdriver.Chrome()
     browser.get(link)
-    select = Select(browser.find_element(By.ID, "cars"))
-    select.select_by_value("2") # searching element 1
-
-    press_button = browser.find_element(By.CSS_SELECTOR, "[type = "submit"]")
+    browser.find_element(By.CSS_SELECTOR, "select#cars").click()
+    browser.find_element(By.CSS_SELECTOR, "[value = 'audi']").click()
+    press_button = browser.find_element(By.CSS_SELECTOR, "[type = 'submit']")
+    press_button.click()
 
 finally:
     time.sleep(5)
