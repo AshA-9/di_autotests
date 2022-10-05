9ee8895663de1b93fc4e18dddd9fb4bf55bd91a7
AshA <b.akhmadjonov@student.inha.uz>
2022-10-05 18:47:16 +0500

    Modifying old code. Getting acknowledged with 'unittest'.

--- scripts/Module 3/Lesson 2/lesson2_step13.py
+++ scripts/Module 3/Lesson 2/lesson2_step13.py
@@ -1,28 +1,38 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-try:
-    browser = webdriver.Chrome()
-    link = "http://suninjuly.github.io/registration1.html"
-    # link = "http://suninjuly.github.io/registration2.html"
+browser = webdriver.Chrome()
+def fncTest(self, link):
     browser.get(link)
-
     input1 = browser.find_element(By.CSS_SELECTOR, ".first_class input[required]")
     input1.send_keys("Ash")
     input2 = browser.find_element(By.CSS_SELECTOR, ".second_class input[required]")
     input2.send_keys("Spring")
     input3 = browser.find_element(By.CSS_SELECTOR, ".third_class input[required]")
     input3.send_keys("ashspring@gmail.com")
-
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
-
     welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
     welcome_text = welcome_text_elt.text
 
-    assert "Congratulations! You have successfully registered!" == welcome_text
+    return self.assertEqual(welcome_text, "Congratulations! You have successfully registered!")
 
-finally:
-    time.sleep(10)
-    browser.quit()
+class TestFrom(unittest.TestCase):
+    def test_registration_form1(self):
+
+        link = "http://suninjuly.github.io/registration1.html"
+        registration_check1 = fncTest(self, link)
+
+
+    def test_registration_form2(self):
+
+        link = "http://suninjuly.github.io/registration2.html"
+        registration_check2 = fncTest(self, link)
+
+if __name__ == "__main__":
+    unittest.main()
+
+time.sleep(5)
+browser.quit()
